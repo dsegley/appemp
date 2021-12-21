cfe1f0cba18611db7236a5a948d70a664c10dff3
DavidSegoviaLey <dsegovia@frd.org.mx>
2021-12-21 17:19:24 -0600

    Sidebar y Header

--- src/app/dashboard/dashboard-components/top-cards/top-cards-data.ts
+++ src/app/dashboard/dashboard-components/top-cards/top-cards-data.ts
@@ -2,7 +2,8 @@ export interface topcard {
     bgcolor: string,
     icon: string,
     title: string,
-    subtitle: string
+    subtitle: string,
+    href: string,
 }
 
 export const topcards: topcard[] = [
@@ -10,26 +11,15 @@ export const topcards: topcard[] = [
     {
         bgcolor: 'success',
         icon: 'bi bi-wallet',
-        title: '$21k',
-        subtitle: 'Yearly Earning'
+        title: 'Nueva Cotización',
+        subtitle: '',
+        href: ''
     },
     {
         bgcolor: 'danger',
         icon: 'bi bi-coin',
-        title: '$1k',
-        subtitle: 'Refund given'
-    },
-    {
-        bgcolor: 'warning',
-        icon: 'bi bi-basket3',
-        title: '456',
-        subtitle: 'Yearly Project'
-    },
-    {
-        bgcolor: 'info',
-        icon: 'bi bi-bag',
-        title: '210',
-        subtitle: 'Weekly Sales'
-    },
-
+        title: 'Registrar Pago',
+        subtitle: '',
+        href: ''
+    }
 ] 
--- src/app/shared/sidebar/menu-items.ts
+++ src/app/shared/sidebar/menu-items.ts
@@ -4,79 +4,15 @@ export const ROUTES: RouteInfo[] = [
  
   {
     path: '/dashboard',
-    title: 'Dashboard',
-    icon: 'bi bi-speedometer2',
+    title: 'Inicio',
+    icon: 'bi bi-house-door',
     class: '',
     extralink: false,
     submenu: []
   },
   {
     path: '/component/alert',
-    title: 'Alert',
-    icon: 'bi bi-bell',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/component/badges',
-    title: 'Badges',
-    icon: 'bi bi-patch-check',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/component/buttons',
-    title: 'Button',
-    icon: 'bi bi-hdd-stack',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/component/card',
-    title: 'Card',
-    icon: 'bi bi-card-text',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/component/dropdown',
-    title: 'Dropdown',
-    icon: 'bi bi-menu-app',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/component/pagination',
-    title: 'Pagination',
-    icon: 'bi bi-dice-1',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/component/nav',
-    title: 'Nav',
-    icon: 'bi bi-pause-btn',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/component/table',
-    title: 'Table',
-    icon: 'bi bi-layout-split',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/about',
-    title: 'About',
+    title: 'Clientes',
     icon: 'bi bi-people',
     class: '',
     extralink: false,
