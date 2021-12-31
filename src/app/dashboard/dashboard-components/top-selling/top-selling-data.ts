25fa03cb61707eccdcd1c8f6c72999fb94f338ad
MayraMed <mayra.mendoza.a82@gmail.com>
2021-12-31 09:55:30 -0600

    topselling

--- src/app/dashboard/dashboard-components/top-selling/top-selling-data.ts
+++ src/app/dashboard/dashboard-components/top-selling/top-selling-data.ts
@@ -2,10 +2,12 @@ export interface Product {
     image: string,
     uname: string,
     gmail: string,
-    productName: string,
+    telefono: number,
+    productName: number,
     status: string,
-    weeks: number,
-    budget: string
+    weeks: string,
+    budget: string,
+
 }
 
 export const TopSelling: Product[] = [
@@ -14,36 +16,40 @@ export const TopSelling: Product[] = [
         image: 'assets/images/users/user1.jpg',
         uname: 'Hanna Gover',
         gmail: 'hgover@gmail.com',
-        productName: 'Flexy React',
-        status: 'danger',
-        weeks: 35,
+        telefono: 9993456574 ,
+        productName: 1,
+        status: '25/SEP/2021',
+        weeks: '25/JUL/2022',
         budget: '95K'
     },
     {
         image: 'assets/images/users/user2.jpg',
         uname: 'Hanna Gover',
         gmail: 'hgover@gmail.com',
-        productName: 'Landing pro React',
-        status: 'info',
-        weeks: 35,
+        telefono: 9993456574  ,
+        productName: 2 ,
+        status: '25/SEP/2021',
+        weeks: '25/JUL/2022',
         budget: '95K'
     },
     {
         image: 'assets/images/users/user3.jpg',
         uname: 'Hanna Gover',
         gmail: 'hgover@gmail.com',
-        productName: 'Elite React	',
-        status: 'warning',
-        weeks: 35,
+        telefono: 9993456574 ,
+        productName: 3 ,
+        status: '25/SEP/2021',
+        weeks: '25/JUL/2022',
         budget: '95K'
     },
     {
         image: 'assets/images/users/user4.jpg',
         uname: 'Hanna Gover',
         gmail: 'hgover@gmail.com',
-        productName: 'Ample React',
-        status: 'success',
-        weeks: 35,
+        telefono: 9993456574 ,
+        productName: 4 ,
+        status: '25/SEP/2021',
+        weeks: '25/JUL/2022',
         budget: '95K'
     },
 
