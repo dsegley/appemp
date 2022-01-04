3674813a6da6f3feaa510ddc4531af8e48ee9b66
MayraMed <mayra.mendoza.a82@gmail.com>
2022-01-04 13:45:13 -0600

    Refactor

--- src/app/agregar-cliente/agregar-cliente.component.ts
+++ src/app/agregar-cliente/agregar-cliente.component.ts
@@ -1,31 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { CpService } from "../services/cp/cp.service";
 
 @Component({
-  selector: 'app-agregar-cliente',
-  templateUrl: './agregar-cliente.component.html',
-  styleUrls: ['./agregar-cliente.component.scss']
+  selector: "app-agregar-cliente",
+  templateUrl: "./agregar-cliente.component.html",
+  styleUrls: ["./agregar-cliente.component.scss"],
 })
 export class AgregarClienteComponent implements OnInit {
+  form: FormGroup;
+  submited = false;
 
-  form: FormGroup
-  submited = false
-
-  constructor(private formBuilder: FormBuilder) { 
+  constructor(private formBuilder: FormBuilder, private cpService: CpService) {
     this.form = this.formBuilder.group({
-      nom_1: ['', Validators.required],
-      nom_2: [''],
-      apellido_pat: ['', Validators.required],
-      apellido_mat: ['', Validators.required],
-      edad: ['', Validators.required],
-      numero_ide: ['', Validators.required],
-      id_cat_ide: ['', Validators.required],
-      correo: ['', Validators.required],
-      telefono: ['', Validators.required],
-    })
+      nom_1: ["", Validators.required],
+      nom_2: [""],
+      apellido_pat: ["", Validators.required],
+      apellido_mat: ["", Validators.required],
+      edad: ["", Validators.required],
+      numero_ide: ["", Validators.required],
+      id_cat_ide: ["", Validators.required],
+      correo: ["", Validators.required],
+      telefono: ["", Validators.required],
+    });
   }
 
   ngOnInit(): void {
+    this.cpService.searchCp("5916").subscribe((data) => {
+      console.log(data);
+    });
   }
-
 }
--- src/app/services/cp/cp.service.ts
+++ src/app/services/cp/cp.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CpService {
 
-  constructor() { }
+
+
+export class CpService {
+   private baseURL = "http://10.60.63.100:5000/v1/"
+  constructor( private httpClient: HttpClient) {  }
+
+public searchCp(query:string) : Observable<any>{
+  const endpoint = this.baseURL + "/v1/cp/?query=" + query
+  return this.httpClient.get(endpoint)
+}
 }
