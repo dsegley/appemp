b3f8e9b402f2505958a0d23ff55f50d7b60912c2
MayraMed <mayra.mendoza.a82@gmail.com>
2022-01-04 12:48:21 -0600

    cp

---
+++ src/app/models/cp.ts
@@ -0,0 +1,11 @@
+export class Cp{
+
+  id_ref_cp!:number;
+  id_cat_asentamientos!: number;
+  nom_cat_asentamientos!: String;
+  id_municipio!:number;
+  cp!: number;
+  nom_entidad!: string;
+  id_estado!: number;
+  nom_estado!: string;
+}
